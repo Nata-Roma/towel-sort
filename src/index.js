764e034ca0544be3d01c4674c97b63c383a652b8
Natalia Romashko <sharkellgm@gmail>
2021-02-26 10:43:59 +0200

    fix: create function for tests passing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if(!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  let arr = [];
+  
+  for(let i = 0; i < matrix.length; i++) {
+    if(i % 2 === 0) {
+      arr = [...arr, ...matrix[i]];
+    } else {
+      matrix[i].reverse();
+      arr = [...arr, ...matrix[i]];
+    }
+  }
+  return arr;
 }
